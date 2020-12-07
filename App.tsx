ebf30b1131863981c4a9b91b688c9ee5c7ace088
Prasanta Barman <prasantabarman06@gmail.com>
2020-12-07 22:21:40 +0530

    fixed husky config to use ts and tsx files moved main app to src directory

--- App.tsx
+++ App.tsx
@@ -1,24 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-const App = (): JSX.Element => {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-
-      <StatusBar style="auto" />
-    </View>
-  );
-};
+import App from './src/App';
 
 export default App;
