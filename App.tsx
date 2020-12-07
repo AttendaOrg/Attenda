e886ef00e42669e2fd42d31f8b637d77147153a2
Prasanta Barman <prasantabarman06@gmail.com>
2020-12-07 22:15:42 +0530

    added eslint and husky for code formatting

--- App.tsx
+++ App.tsx
@@ -2,15 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,3 +10,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const App = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default App;
