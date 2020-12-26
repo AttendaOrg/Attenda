16038a5219be87300620422f6920738c5f5abe35
Prasanta Barman <prasantabarman06@gmail.com>
2020-12-26 13:48:16 +0530

    fix: made only the close btn clickable in SimpleCloseNavigationOptions

--- src/components/templates/SimpleCloseNavigationOption.tsx
+++ src/components/templates/SimpleCloseNavigationOption.tsx
@@ -2,17 +2,29 @@ import React from 'react';
 import { StackNavigationOptions } from '@react-navigation/stack';
 import { StatusBar, Text, TouchableOpacity, View } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import { lightColor } from '../../util/Colors';
 
 const SimpleCloseNavigationOptions: StackNavigationOptions = {
   headerTransparent: true,
-  header: props => (
-    <View style={{ marginTop: (StatusBar.currentHeight || 0) / 2 }}>
-      <TouchableOpacity onPress={() => props.navigation.goBack()}>
-        <MaterialIcons name="close" size={24} style={{ margin: 16 }} />
-      </TouchableOpacity>
-    </View>
-  ),
+  title: '',
+  headerLeft: ({ tintColor }) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const navigation = useNavigation();
+
+    return (
+      <View style={{ marginTop: (StatusBar.currentHeight || 0) / 2 }}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <MaterialIcons
+            name="close"
+            size={24}
+            color={tintColor}
+            style={{ margin: 16 }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  },
 };
 
 export const NoHeaderNavigationOptions: StackNavigationOptions = {
