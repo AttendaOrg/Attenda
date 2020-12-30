7b4e904e256f6ddb3863af17cf1468ee45edbbd1
apurbar06 <apurbar06@gmail.com>
2020-12-30 18:49:19 +0530

    add single button popup in sign up page

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Linking } from 'react-native';
 import {
   StackNavigationOptions,
@@ -8,6 +8,7 @@ import { RootStackParamList } from '../App';
 import SignUpPage from '../components/organisms/AppIntro/SignUp';
 import SimpleCloseNavigationOptions from '../components/templates/SimpleCloseNavigationOption';
 import { PRIVACY_POLICY_URL, TERMS_URL } from '../util/constant';
+import SingleButtonPopup from '../components/molecules/SingleButtonPopup';
 
 type Props = StackScreenProps<RootStackParamList, 'SignUp'>;
 
@@ -20,13 +21,26 @@ export const SignUpPageNavigationOptions: StackNavigationOptions = SimpleCloseNa
 // };
 
 const SignUpPagePage: React.FC<Props> = ({ navigation }): JSX.Element => {
+  const [showConfirmEmailPopup, setShowConfirmEmailPopup] = useState(false);
+
   return (
-    <SignUpPage
-      onSignUpClick={navigation.goBack}
-      onSubmit={() => null}
-      onTermsClick={() => Linking.openURL(TERMS_URL)}
-      onPrivacyPolicyClick={() => Linking.openURL(PRIVACY_POLICY_URL)}
-    />
+    <>
+      <SignUpPage
+        onSignUpClick={navigation.goBack}
+        onSubmit={() => setShowConfirmEmailPopup(true)}
+        onTermsClick={() => Linking.openURL(TERMS_URL)}
+        onPrivacyPolicyClick={() => Linking.openURL(PRIVACY_POLICY_URL)}
+      />
+
+      <SingleButtonPopup
+        visible={showConfirmEmailPopup}
+        title="Confirm Email"
+        text="We have sent you a mail. Please confirm it."
+        buttonText="Ok"
+        onDismiss={() => setShowConfirmEmailPopup(false)}
+        onButtonClick={() => null}
+      />
+    </>
   );
 };
 
