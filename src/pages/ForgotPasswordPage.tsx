9fe0bb1bc1f6fb27eb505ec1d2b502bca944a4be
Prasanta Barman <prasantabarman06@gmail.com>
2021-02-03 15:21:38 +0530

    feat: made forgot password email error inline

--- src/pages/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage.tsx
@@ -10,6 +10,7 @@ import SingleButtonPopup from '../components/molecules/SingleButtonPopup';
 import GlobalContext from '../context/GlobalContext';
 import { authApi } from '../api/AuthApi';
 import { BasicErrors, convertErrorToMsg } from '../api/BaseApi';
+import { isValidEmail } from '../util';
 // import { useContext } from 'react';
 
 type Props = StackScreenProps<RootStackParamList, 'ForgotPassword'>;
@@ -22,17 +23,38 @@ export const ForgotPasswordNavigationOptions: StackNavigationOptions = SimpleClo
 const ForgotPasswordPage: React.FC<Props> = ({ navigation }): JSX.Element => {
   const globalContext = useContext(GlobalContext);
   const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [hasTriedSubmitting, setHasTriedSubmitting] = useState(false);
   const [showSuccessEmailSendPopup, setShowSuccessEmailSendPopup] = useState(
     false,
   );
-  const [showFailedEmailSendPopup, setShowFailedEmailSendPopup] = useState(
-    false,
-  );
-  const [errorMessage, setErrorMessage] = useState(
-    'Something went wrong. Please try again later.',
-  );
+
+  const revalidateError = (_email: string): boolean => {
+    if (!isValidEmail(_email)) {
+      // if there is error field is empty only then update it
+      // if not we don't need to update it because it could cause
+      // an un necessary re-render
+      if (emailError === '') setEmailError('Please enter a valid email.');
+
+      return false;
+    }
+    if (emailError !== '') setEmailError('');
+
+    return true;
+  };
+
+  const updateEmail = (_email: string) => {
+    if (hasTriedSubmitting) revalidateError(_email);
+    setEmail(_email);
+  };
 
   const handleOnSend = async () => {
+    if (hasTriedSubmitting === false) setHasTriedSubmitting(true);
+    // if the error validation fails we don't need to call the api
+    // because the validation will fail only if we have a invalid
+    if (!revalidateError(email)) return;
+
     globalContext.changeSpinnerLoading(true);
 
     const [success, error] = await authApi.sendPasswordResetEmail(
@@ -47,17 +69,14 @@ const ForgotPasswordPage: React.FC<Props> = ({ navigation }): JSX.Element => {
     } else {
       switch (error) {
         case BasicErrors.AUTH_USER_NOT_FOUND:
-          setShowFailedEmailSendPopup(true);
-          setErrorMessage(
+          setEmailError(
             'This email address not registered with us. Please create an account',
           );
           break;
         case BasicErrors.INVALID_EMAIL:
-          setShowFailedEmailSendPopup(true);
-          setErrorMessage('Please check the email you have entered');
+          setEmailError('Please check the email you have entered');
           break;
         default:
-          setShowFailedEmailSendPopup(true);
           setErrorMessage('Something went wrong. Please try again later.');
           break;
       }
@@ -69,9 +88,10 @@ const ForgotPasswordPage: React.FC<Props> = ({ navigation }): JSX.Element => {
     <>
       <ForgotPassword
         email={email}
-        onEmailChange={setEmail}
+        onEmailChange={updateEmail}
         // onSend={() => setShowSuccessEmailSendPopup(true)}
         onSend={handleOnSend}
+        errors={{ emailError }}
       />
       <SingleButtonPopup
         visible={showSuccessEmailSendPopup}
@@ -82,12 +102,12 @@ const ForgotPasswordPage: React.FC<Props> = ({ navigation }): JSX.Element => {
         onButtonClick={() => navigation.navigate('SignIn')}
       />
       <SingleButtonPopup
-        visible={showFailedEmailSendPopup}
+        visible={errorMessage !== ''}
         title="Ooops..."
         text={errorMessage}
         buttonText="Ok"
-        onDismiss={() => setShowFailedEmailSendPopup(false)}
-        onButtonClick={() => setShowFailedEmailSendPopup(false)}
+        onDismiss={() => setErrorMessage('')}
+        onButtonClick={() => setErrorMessage('')}
       />
     </>
   );
